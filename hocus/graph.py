aa8465d2a58a926cb02ab3871f3a859e433f53da
woitee <woitee@gmail.com>
2017-06-11 20:24:52 +0200

    Start of solver

--- hocus/graph.py
+++ hocus/graph.py
@@ -13,6 +13,22 @@ class Direction(IntEnum):
         return Direction((self + 3) % 6)
 
 
+# Face enum compatible with Direction
+class Face(IntEnum):
+    TOP = 0
+    BACK = 1
+    RIGHT = 2
+    BOTTOM = 3
+    FRONT = 4
+    LEFT = 5
+
+    def opposite(self):
+        return Face((self + 3) % 6)
+
+    def adjacents(self):
+        return [Face((self + i) % 6) for i in [1, 2, 4, 5]]
+
+
 class Node:
     def __init__(self, x, y, directions, neighbors=None):
         self.location = [x, y]
--- hocus/solver.py
+++ hocus/solver.py
@@ -1,2 +1,75 @@
+from hocus.graph import Direction, Face
+from collections import deque, namedtuple
+
+
+PathPart = namedtuple('PathPart', ['node_from', 'node_to', 'dir', 'face'])
+
+
 def solve(graph):
-    return None
+    """
+    Explores all reachable parts of graph from all starting points
+    Args:
+        graph: hocus.graph
+
+    Returns:
+
+    """
+
+    explored = []
+
+    def opposite_path_part(ppart):
+        return PathPart(ppart.node_to, ppart.node_from, ppart.dir.opposite(), ppart.face)
+
+    def mark_explored(ppart):
+        explored.append(ppart)
+        explored.append(opposite_path_part(ppart))
+
+    def is_explored(ppart):
+        return ppart in explored
+
+    # find nodes with degree 0
+    starts = [node for node in graph.nodes if len(node.directions) == 1]
+    start = starts[0]
+    start_dir = start.directions[0]
+    q = deque()
+    for face in Face(start_dir).adjacents():
+        path_part = PathPart(start, start.neighbors[start_dir], start_dir, face)
+        q.append(path_part)
+        mark_explored(path_part)
+
+    iter = 0
+    while len(q) > 0:
+        node_from, node_to, direction, face = q.popleft()
+        candidates = []
+        # print("Directions are {}".format(node_to.directions, Direction(face)))
+
+        if Direction(face) in node_to.directions:
+            candidates.append(PathPart(node_to, node_to.neighbors[face], face, direction.opposite()))
+        else:
+            for new_dir in face.adjacents():
+                if Direction(new_dir) in node_to.directions:
+                    # print("Appending {} {} {} {}".format(
+                    #     node_to.location,
+                    #     [Direction(i) for [i, _] in enumerate(node_to.neighbors) if _ is not None],
+                    #     new_dir,
+                    #     face
+                    # ))
+                    candidates.append(PathPart(node_to, node_to.neighbors[new_dir], new_dir, face))
+
+        for path_part in candidates:
+            if not is_explored(path_part):
+                # print("Exploring from {} to {} in {} on {}".format(
+                #     path_part.node_from.location,
+                #     path_part.node_to.location,
+                #     str(Direction(path_part.dir)),
+                #     str(Face(path_part.face))
+                # ))
+                mark_explored(path_part)
+                q.append(path_part)
+
+        if ++iter % 100 == 0:
+            print("Explored {} parts".format(len(explored)))
+
+
+    # print(len(explored))
+
